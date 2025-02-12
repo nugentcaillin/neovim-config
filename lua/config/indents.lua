4ce57cc21ffc4c001e99c0734e33d6ae7d87f452
Caillin Nugent <nugentcaillin@gmail.com>
2025-02-12 14:54:25 +1100

    added snippets

--- lua/config/indents.lua
+++ lua/config/indents.lua
@@ -10,6 +10,10 @@ vim.api.nvim_create_autocmd("FileType", {
 	pattern = "javascript",
 	command = "setlocal shiftwidth=2 tabstop=2",
 })
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "lua",
+	command = "setlocal shiftwidth=2 tabstop=2",
+})
 
 vim.wo.breakindent = true
 vim.opt.breakindentopt = {'shift:2'}
--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,5 +1,58 @@
 return {
+	{
     "williamboman/mason.nvim",
+		lazy=false,
+		config = function ()
+			require("mason").setup()
+		end,
+	},
+	{
     "williamboman/mason-lspconfig.nvim",
+		lazy=false,
+		opts= {
+			auto_install = true,	
+			ensure_installed = {
+				"html",
+				"lua_ls",
+				"rust_analyzer",
+				"cssls",
+				"eslint",
+				"vtsls",
+			}
+		}
+	},
+	{
     "neovim/nvim-lspconfig",
+		lazy = false,
+		config = function ()
+			local lspconfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			lspconfig.html.setup({
+				capabilities = capabilities
+			})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities
+			})
+			lspconfig.rust_analyzer.setup({
+				capabilities = capabilities,
+    		settings = {
+      		['rust-analyzer'] = {
+        		diagnostics = {
+         		enable = true;
+        		}
+      		}
+    		}
+			})
+			lspconfig.cssls.setup({
+				capabilities = capabilities
+			})
+			lspconfig.eslint.setup({
+				capabilities = capabilities
+			})
+			lspconfig.vtsls.setup({
+				capabilities = capabilities
+			})
+		end,
+	},
 }
