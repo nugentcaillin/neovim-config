27c886db3b8af371a41c48ce7679f5ef0bc9cb56
Caillin Nugent <nugentcaillin@gmail.com>
2025-02-12 11:33:46 +1100

    Added support for kitty transparency
    changed background colour to match kitty default

--- lua/config/appearance.lua
+++ lua/config/appearance.lua
@@ -1,2 +1,18 @@
 -- setup colorscheme
-vim.cmd("colorscheme tokyonight-storm")
+
+require("tokyonight").setup({
+  -- use the night style
+  style = "storm",
+  -- disable italic for functions
+  styles = {
+    functions = {}
+  },
+  -- Change the "hint" color to the "orange" color, and make the "error" color bright red
+  on_colors = function(colors)
+    colors.bg = "#000000"
+    colors.bg_sidebar = "#000000"
+  end
+})
+
+vim.cmd("colorscheme tokyonight")
+
--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -2,5 +2,6 @@ return {
   "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
-  opts = {},
+  opts = {
+  },
 }
